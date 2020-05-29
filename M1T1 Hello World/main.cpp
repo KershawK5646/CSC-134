1dcd30e6e50f1cad7208e99a205a3d021acaf3f3
KershawK5646 <43454481+KershawK5646@users.noreply.github.com>
2020-05-29 17:14:15 -0400

    Module 1 Tutorial 1 Assignment
    Created a C++ program that prints "Hello World" and greets me.
    This was written and downloaded using repl.it

---
+++ M1T1 Hello World/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// M1T1
+// Hello World
+// Kyler Kershaw
+// 05/29/2020
+int main() {
+  cout << "Hello World!\n";
+
+  // Personalization:
+  // Adding my name to the program
+  string userName = "Kyler";
+  cout << "Nice to meet you, " << userName << "." << endl;
+
+}
