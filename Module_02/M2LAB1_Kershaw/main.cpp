d79c65a3b5bf2a6196e6e35015fe5b64d03668aa
KershawK5646 <43454481+KershawK5646@users.noreply.github.com>
2020-06-06 11:54:27 -0400

    Completed M2LAB1
    Completed the lab.
    This program calculates the volume and material cost of a create. It then uses those values to calculate a charge to a customer, profit, then displays them to the user.

---
+++ Module_02/M2LAB1_Kershaw/main.cpp
@@ -0,0 +1,42 @@
+// CSC 134
+// M2LAB1
+// Kyler Kershaw
+// 06/06/2020
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  // Constants for cost and amount charged
+  const double COST_PER_CUBIC_FOOT = 0.23;
+  const double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+  // Variables
+  double length, width, height, volume, cost, charge, profit;
+
+  // Set the desited output formatting for numbers.
+  cout << setprecision(2) << fixed << showpoint;
+
+  // Prompt the user for crate dimensions
+  cout << "Enter the dimensions of the crate in feet: \n";
+  cout << "Length: ";
+  cin >> length;
+  cout << "Width: ";
+  cin >> width;
+  cout << "Height: ";
+  cin >> height;
+
+  // Calculate the crate's volume, the cost,  the charge, and profit
+  volume = length * width * height;
+  cost = volume * COST_PER_CUBIC_FOOT;
+  charge  = volume * CHARGE_PER_CUBIC_FOOT;
+  profit = charge - cost;
+
+  // Display the calculated data
+  cout << "The volume of the crate is: ";
+  cout << volume << " cubic feet. \n";
+  cout << "Cost ot build: " << cost << endl;
+  cout << "Charge to customer: $" << charge << endl;
+  cout << "Profit: $" << profit << endl;
+  return 0;
+}
